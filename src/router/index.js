42d9aebf4fb3f26836900a1647f5d75f126e55bd
戒尺 <itxiao6@qq.com>
2019-08-26 22:52:51 +0800

    主页面路由

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,10 @@ const _import = require('./_import_' + process.env.NODE_ENV);
 import Page404 from '@/views/errorPages/Page404'
 import Page500 from '@/views/errorPages/Page500'
 /**
+ * Master
+ */
+import Full from '@/containers/Full';
+/**
  * 登录页面
  */
 const Login = _import('login/index');
@@ -29,6 +33,13 @@ export const constantRouterMap = [
         path: '/login',
         component: Login,
         hidden: true
+    },
+    {
+        path: '/',
+        // redirect: '/dashboard',
+        name: '首页',
+        component: Full,
+        hidden: false,
     },
     /**
      * 错误页面
