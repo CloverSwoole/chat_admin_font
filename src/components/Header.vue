f1ab0ce92b79d5217803dfbcc88ccc83278cc35e
戒尺 <itxiao6@qq.com>
2019-08-30 16:49:14 +0800

    删除头部内容

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,123 +1,118 @@
 <template>
-  <navbar>
-    <button class="navbar-toggler mobile-sidebar-toggler d-lg-none" type="button" @click="mobileSidebarToggle">&#9776;</button>
-    <a class="navbar-brand" ></a>
-    <ul class="nav navbar-nav ml-auto">
+    <navbar>
+        <a class="navbar-brand">后台管理</a>
+        <ul class="nav navbar-nav ml-auto">
+            <Dropdown class="nav-item">
+                <a href="javascript:void(0)">
+                    <span slot="button">
+                        <img src="static/img/avatars/6.jpg" class="img-avatar" alt="o">
+                        <span class="d-md-down-none">admin</span>
+                    </span>
+                </a>
+                <Dropdown-menu slot="list">
+                    <Dropdown-item>
+                        <p class="dropdown-itemp">
+                            <Icon type="alert"></Icon>
+                            Updates<span class="badge badge-info">42</span>
+                        </p>
+                    </Dropdown-item>
+                    <Dropdown-item>
+                        <p class="dropdown-itemp">
+                            <Icon type="chatbox-working"></Icon>
+                            Messages<span class="badge badge-success">42</span>
+                        </p>
 
-      <li class="nav-item d-md-down-none">
-        <a class="nav-link" ><Icon type="android-notifications" size="20"></Icon><span class="badge badge-pill badge-danger">5</span></a>
-      </li>
+                    </Dropdown-item>
+                    <Dropdown-item>
+                        <p class="dropdown-itemp">
+                            <Icon type="chatbox-working"></Icon>
+                            Messages<span class="badge badge-danger">42</span>
+                        </p>
 
+                    </Dropdown-item>
+                    <Dropdown-item divided>
+                        <p class="dropdown-itemp">
+                            <Icon type="android-contact"></Icon>
+                            Profile
+                        </p>
 
-      <Dropdown class="nav-item">
-        <a href="javascript:void(0)">
-           <span slot="button">
-          <img src="static/img/avatars/6.jpg" class="img-avatar" alt="o">
-          <span class="d-md-down-none">admin</span>
-          </span>
-        </a>
-        <Dropdown-menu slot="list">
-            <Dropdown-item>
-              <p class="dropdown-itemp"><Icon type="alert"></Icon>Updates<span class="badge badge-info">42</span></p>
+                    </Dropdown-item>
+                    <Dropdown-item>
+                        <p class="dropdown-itemp">
+                            <Icon type="android-settings"></Icon>
+                            Settings
+                        </p>
+                    </Dropdown-item>
 
+                    <Dropdown-item><a href="" @click="Logout">
+                        <p class="dropdown-itemp">
+                            <Icon type="power"></Icon>
+                            Logout
+                        </p>
+                    </a></Dropdown-item>
 
-            </Dropdown-item>
-            <Dropdown-item>
-              <p class="dropdown-itemp"><Icon type="chatbox-working"></Icon>Messages<span class="badge badge-success">42</span></p>
-
-            </Dropdown-item>
-             <Dropdown-item>
-              <p class="dropdown-itemp">  <Icon type="chatbox-working"></Icon>Messages<span class="badge badge-danger">42</span></p>
-
-           </Dropdown-item>
-              <Dropdown-item divided>
-              <p class="dropdown-itemp"><Icon type="android-contact"></Icon> Profile</p>
-
-              </Dropdown-item>
-            <Dropdown-item >
-              <p class="dropdown-itemp"><Icon type="android-settings"></Icon> Settings</p>
-              </Dropdown-item>
-
-                 <Dropdown-item > <a href="" @click="Logout"  ><p  class="dropdown-itemp"><Icon type="power"></Icon>Logout</p></a></Dropdown-item>
-
-        </Dropdown-menu>
-    </Dropdown>
-
-
-      <li class="nav-item d-md-down-none">
-        <a class="nav-link navbar-toggler aside-menu-toggler"  @click="asideToggle">&#9776;</a>
-      </li>
-    </ul>
-  </navbar>
+                </Dropdown-menu>
+            </Dropdown>
+        </ul>
+    </navbar>
 </template>
 <script>
 
-import navbar from './Navbar'
+    import navbar from './Navbar'
 
-/**
- * 顶部
- */
-export default {
-  name: 'header',
-  components: {
-    navbar,
+    /**
+     * 顶部
+     */
+    export default {
+        name: 'header',
+        components: {
+            navbar,
 
-  },
-  methods: {
-    Logout(e){
-         e.preventDefault();
-         this.$store.dispatch('LogOut').then(() => {
-                this.$router.push({ path: '/login' });
-              }).catch(err => {
-                this.$message.error(err);
-              });
-    },
-    click () {
-      // do nothing
-    },
-    sidebarToggle (e) {
-      e.preventDefault()
-      document.body.classList.toggle('sidebar-hidden')
-    },
-    sidebarMinimize (e) {
-      e.preventDefault()
-
-      document.body.classList.toggle('sidebar-minimized')
-    },
-    mobileSidebarToggle (e) {
-      e.preventDefault()
-
-      document.body.classList.toggle('sidebar-mobile-show')
-    },
-    asideToggle (e) {
-      e.preventDefault()
-      document.body.classList.toggle('aside-menu-hidden')
+        },
+        methods: {
+            Logout(e) {
+                e.preventDefault();
+                this.$store.dispatch('LogOut').then(() => {
+                    this.$router.push({path: '/login'});
+                }).catch(err => {
+                    this.$message.error(err);
+                });
+            },
+            click() {
+                // do nothing
+            },
+        }
     }
-  }
-}
 </script>
 
 <style type="text/css" scoped>
-  .dropdown-itemp{
-    text-align: left;
-    font-size: 15px;
-    padding: 10px;
-  }
- .header-item .ivu-dropdown-item{
-  padding: 15px;
-}
-  .header-item{
-    width: 130px;
-    /*background-color: #20a8d8;*/
-    /*background-color: white;*/
+    .dropdown-itemp {
+        text-align: left;
+        font-size: 15px;
+        padding: 10px;
+    }
 
-    height: 55px;
+    .navbar-nav {
+        margin-right: 20px;
+    }
+
+    .header-item .ivu-dropdown-item {
+        padding: 15px;
+    }
+
+    .header-item {
+        width: 130px;
+        /*background-color: #20a8d8;*/
+        /*background-color: white;*/
+
+        height: 55px;
 
 
-  }
-  .header-item a{
-        color:white !important;
+    }
 
-  }
+    .header-item a {
+        color: white !important;
+
+    }
 
 </style>
